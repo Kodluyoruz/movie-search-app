e5018b120bbb45e243b79f74910ab4c4c47ab0da
Emre <48946077+FuzEmo@users.noreply.github.com>
2021-02-08 20:26:27 +0300

    Favorites added
    Co-Authored-By: Sinan Işık <sinan.isik@ug.bilkent.edu.tr>

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,7 +1,30 @@
-import React from 'react';
-
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
+import Card from "../components/Card";
+import "../styles/Layout.css";
 const Favorite = () => {
-  return <div></div>;
+  const { favorite, favoriteHandler } = useContext(MovieContext);
+
+  return (
+    <>
+      {favorite?.length > 0 ? (
+        <div className="layout">
+          {favorite.map((movie) => (
+            <Card
+              key={movie.imdbID}
+              image={movie.Poster}
+              title={movie.Title}
+              year={movie.Year}
+              onClick={() => favoriteHandler(movie)}
+              isFavorite={movie.isFavorite}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="favorite_warning"> There is no Favorite Movie </div>
+      )}
+    </>
+  );
 };
 
 export default Favorite;
